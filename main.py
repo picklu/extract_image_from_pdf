154989e8076cb4b9998181e14349e56787927d8b
Subrata Sarker <subrata_sarker@yahoo.com>
2022-10-23 20:01:34 +0600

    Define extract_image function to extract image from pdf file

--- main.py
+++ main.py
@@ -4,11 +4,24 @@ from glob import glob
 from PyPDF2 import PdfReader
 
 
+def extract_image(idx, pdf_file, outPath):
+    pdf = PdfReader(pdf_file)
+    print(f"==> [{idx}] Extracting from {pdf_file}.")
+    for page in pdf.pages:
+        for img in page.images:
+            img_file = path.join(outPath, "pic_{0}_{1}".format(idx, img.name))
+            print(f"==> [{idx}] Saving to {img_file}.")
+            with open(img_file, "wb") as fp:
+                fp.write(img.data)
 
-img_folder = config('IMG_FOLDER')
-img_file_pattern = path.join(img_folder, "Scan *.pdf")
 
 
-
-
-files = glob(img_file_pattern)
+if __name__ == "__main__":
+    pdf_folder = config('PDF_FOLDER')
+    img_folder = config("IMG_FOLDER")
+    pdf_file_pattern = path.join(pdf_folder, "Scan *.pdf")
+    pdf_files = glob(pdf_file_pattern)
+    print(f"==> Total {len(pdf_files)} files found.")
+    for idx, pdf_file in enumerate(pdf_files):
+        extract_image(idx, pdf_file, img_folder)
+    print("==> done!")
